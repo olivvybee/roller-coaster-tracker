dfeadc1990d231817d52821f0843b078f015abe7
Liv Flowers <oliviaflowers93@gmail.com>
2024-11-21 20:33:28 +0000

    Allow less precise dates

--- src/addCoaster.ts
+++ src/addCoaster.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { z } from 'zod';
 
+const dateRegex = /^\d{4}(-\d{2})?(-\d{2})?$/;
+
 export const addCoasterSchema = z.object({
 	parkId: z.string(),
 	name: z.string(),
 	ridden: z.optional(z.boolean()),
-	opened: z.string().date(),
-	closed: z.optional(z.string().date()),
+	opened: z.string().regex(dateRegex),
+	closed: z.optional(z.string().regex(dateRegex)),
 	latitude: z.number(),
 	longitude: z.number(),
 	rcdb: z.string(),
